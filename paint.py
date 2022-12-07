39fc5950b0cc780cff272362180f4c7c949a34a0
maic <maic-ai>
2022-12-07 14:30:20 +0330

    Update paint file

--- paint.py
+++ paint.py
@@ -6,28 +6,28 @@ drawing = False
 ix, iy = -1, -1
 img = None
 orig_img = None
-drawing_img = None
+paint_img = None
 
 
 # mouse callback function
 def draw_rectangle(event, x, y, flags, param):
-    global ix, iy, drawing, img, orig_img, drawing_img
+    global ix, iy, drawing, img, orig_img, paint_img
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix, iy = x, y
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
-            img = drawing_img.copy()
+            img = paint_img.copy()
             cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 2)
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        cv2.rectangle(drawing_img, (ix, iy), (x, y), (0, 255, 0), 2)
-        img = drawing_img.copy()
+        cv2.rectangle(paint_img, (ix, iy), (x, y), (0, 255, 0), 2)
+        img = paint_img.copy()
 
 
 orig_img = np.ones((600, 800, 3), np.uint8)
 img = orig_img.copy()
-drawing_img = orig_img.copy()
+paint_img = orig_img.copy()
 
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_rectangle)
@@ -35,7 +35,7 @@ while(1):
     cv2.imshow('image', img)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('c'):
-        drawing_img = orig_img.copy()
+        paint_img = orig_img.copy()
         img = orig_img.copy()
     elif k == 27:
         break
